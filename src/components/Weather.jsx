4fd0bd13b556e9a99ee6d8ee66ee621937095b0c
spring <springsquash93@gmail.com>
2023-04-28 12:22:03 -0400

    Add input placeholder

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -15,7 +15,7 @@ export default function Weather(props) {
 
             <div className='flex justify-end mb-14'>
 
-              <input className="bg-transparent border-b border-gray-350 hover:animate-pulse focus:outline-none flex-grow mr-5 mt-8 text-white ml-5" id="inputTbx" type="text"></input>
+              <input placeholder='Another location' className="bg-transparent border-b border-gray-350 hover:animate-pulse focus:outline-none flex-grow mr-5 mt-8 text-white ml-5" id="inputTbx" type="text"></input>
               <button className="flex-shrink-0 flex items-center justify-center w-14 h-14 mr-4 rounded-full bg-yellow-50 hover:bg-sky-200 mt-4" onClick={() => props.onCityChange(document.getElementById("inputTbx").value)}><AiOutlineSearch size={35} /></button>
 
             </div>
