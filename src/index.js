6bfbb52e1335c4fb7425eb9e398dcf32b563e804
Spring <Springsquash93@gmail.com>
2024-06-13 23:07:24 -0400

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import WeatherApp from './components/WeatherApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +9,3 @@ root.render(
     <WeatherApp />
   </React.StrictMode>
 );
-
-reportWebVitals();
